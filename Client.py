93d85b4db31629b342a6ff5bf6ebe302ed0dfd74
jerrickzhu <jerrickzhu@gmail.com>
2024-02-28 09:00:38 -0800

    First commit. Start Client class

---
+++ Client.py
@@ -0,0 +1,8 @@
+import socket
+
+class Socket():
+  def __init__(self, host_name: str, port_to_connect: int) -> None:
+    str: self.host_name = host_name
+    int: self.port_to_connect = port_to_connect
+  
+  
