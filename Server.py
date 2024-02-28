24e5d9babcd74ada4ada53cc035f22a65f1cb21a
jerrickzhu <jerrickzhu@gmail.com>
2024-02-28 12:25:49 -0800

    Add initialize_socket function

--- Server.py
+++ Server.py
@@ -3,7 +3,14 @@ import socket
 class Server():
   def __init__(self) -> None:
     self.host_address: str = "0.0.0.0"
-    self.PORT: int = 5000
+    self.PORT: int = 5001
     self.server_socket: socket = None
     self.number_of_listens: int = int(input("Please enter the number of people the server can listen to at a time: "))
 
+  def initialize_socket(self) -> None:
+    try:
+      self.server_socket = socket.socket()
+      self.server_socket.bind((self.host_address, self.PORT))
+      print("The socket instance has successfully been instantiated.")
+    except Exception as e:
+      print(f"Initializing a socket instance has failed: {e}")
