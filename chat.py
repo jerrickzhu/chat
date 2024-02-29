a29941aab888bd0b701859838feb7fca8fba9450
jerrickzhu <jerrickzhu@gmail.com>
2024-02-28 21:24:14 -0800

    Edit Chat to only handle messages shown

--- chat.py
+++ chat.py
@@ -6,23 +6,6 @@ class Chat():
     Chat class to handle all chat functions.
   """
   @staticmethod
-  def handle_messages(server: Server, client: socket.socket):
-    """ Handles messages across all clients. 
-      Parameters:
-        client - socket object that is an instance of a client
-    """
-    try:
-      while True:
-        message: bytes = client.recv(1024).decode()
-        if not message or not isinstance(message, bytes):
-          break
-        Chat.show_messages(message.encode())
-    except (ConnectionAbortedError, ConnectionResetError):
-      server.clients.remove(client)
-      print(f"A client has disconnected")
-      client.close()
-
-  @staticmethod
   def show_messages(clients, message: str) -> None:
     """ Shows messages to all clients. """
     try:
