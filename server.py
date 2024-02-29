36bacf8c8ab3017fe712265e97174049c2b2b923
jerrickzhu <jerrickzhu@gmail.com>
2024-02-28 19:13:21 -0800

    Fix bug with thread target

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from typing import List
 from threading import Thread
+from chat import Chat
 import socket
 import threading
 
@@ -33,7 +34,7 @@ class Server():
         client, client_address = self.server_socket.accept()
         print(f"We have a new client from {client_address}")
         self.clients.append(client)
-        client_thread: Thread = Thread(target=self.handle_messages, args=(client,))
+        client_thread: Thread = Thread(target=Chat.handle_messages, args=(client,))
         client_thread.start()
 
     except Exception as e:
