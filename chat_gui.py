8a2b13459dcec76dcff1ea7b72425ba29fe664cc
jerrickzhu <jerrickzhu@gmail.com>
2024-03-01 21:09:13 -0800

    Fix format

--- chat_gui.py
+++ chat_gui.py
@@ -8,41 +8,41 @@ import threading
 
 class ChatGUI(App):
   def __init__(self, client, **kwargs):
-      super().__init__(**kwargs)
-      self.client = client
-      self.message_input = None
-      self.chat_display = None
+    super().__init__(**kwargs)
+    self.client = client
+    self.message_input = None
+    self.chat_display = None
 
   def build(self):
-      layout = BoxLayout(orientation='vertical')
+    layout = BoxLayout(orientation='vertical')
 
-      self.chat_display = TextInput(readonly=True, multiline=True)
-      layout.add_widget(self.chat_display)
+    self.chat_display = TextInput(readonly=True, multiline=True)
+    layout.add_widget(self.chat_display)
 
-      self.message_input = TextInput(multiline=False)
-      layout.add_widget(self.message_input)
+    self.message_input = TextInput(multiline=False)
+    layout.add_widget(self.message_input)
 
-      send_button = Button(text='Send')
-      send_button.bind(on_press=self.send_message)
-      layout.add_widget(send_button)
+    send_button = Button(text='Send')
+    send_button.bind(on_press=self.send_message)
+    layout.add_widget(send_button)
 
-      threading.Thread(target=self.client.start_client).start()
+    threading.Thread(target=self.client.start_client).start()
 
-      # Check for incoming messages periodically
-      Clock.schedule_interval(self.receive_messages, 1.0)
+    # Check for incoming messages periodically
+    Clock.schedule_interval(self.receive_messages, 1.0)
 
-      return layout
+    return layout
 
   def send_message(self, instance) -> None:
-      message = self.message_input.text.strip()
-      if message:
-          threading.Thread(target=self.client.send_message, args=(message,)).start()
-          self.message_input.text = ''
+    message = self.message_input.text.strip()
+    if message:
+        threading.Thread(target=self.client.send_message, args=(message,)).start()
+        self.message_input.text = ''
 
   def receive_messages(self, dt) -> None:
-      message: str = self.client.get_message()
-      if message:
-          self.chat_display.text += f'\n{message}'
+    message: str = self.client.get_message()
+    if message:
+        self.chat_display.text += f'\n{message}'
 
 if __name__ == '__main__':
 
