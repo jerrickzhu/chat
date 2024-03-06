cc45cc0504eadca73653aae06bacc7438cc706b0
jerrickzhu <jerrickzhu@gmail.com>
2024-03-06 00:56:01 -0800

    Finish chat

--- main_gui.py
+++ main_gui.py
@@ -1,5 +1,4 @@
 from kivy.uix.boxlayout import BoxLayout
-from drawing_gui import DrawingWidget
 from chat_gui import ChatGUI
 
 class MainGUI(BoxLayout):
@@ -7,8 +6,6 @@ class MainGUI(BoxLayout):
     super().__init__(**kwargs)
     self.orientation = 'horizontal'
 
-    drawing_widget = DrawingWidget()
     chat_gui = ChatGUI(client)
 
-    self.add_widget(drawing_widget)
     self.add_widget(chat_gui)
